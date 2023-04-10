263b3892bfd75de11d9cf56e7281c566c485b316
Lincoln Berto <lincoln.santospro@gmail.com>
2023-04-10 19:27:22 -0300

    add atualizações

--- JAVASCRIPT/exercicio4/visitando_novas_cidades.js
+++ JAVASCRIPT/exercicio4/visitando_novas_cidades.js
@@ -7,4 +7,13 @@ let continuar = prompt('Você visitou alguma cidade? (Sim ou Não)');
 while (continuar === 'Sim') {
   let cidade = prompt('Qual é o nome da cidade?');
   cidades += ' - ' + cidade + '\n';
+  contagem++;
+  continuar = prompt('Você visitou alguma outra cidade? Sim ou Não');
 }
+
+alert(`
+  Turista: ${turista}
+  Quantidade de Cidades viajadas: ${contagem}
+  Cidades visitadas: 
+  ${cidades}
+`);
