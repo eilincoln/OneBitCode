5a89673c3a08430ab9c2a47761bfd551b7f4521c
Lincoln Berto <lincoln.santospro@gmail.com>
2023-04-23 23:33:50 -0300

    atualização material

--- JAVASCRIPT/exercicio_06/tabuada.js
+++
@@ -1,6 +0,0 @@
-let numero_tabuada = prompt("Digite o número a ser multiplicado")
-
-for(let i = 0; i <= 20; i++) {
-    let resultado = numero_tabuada * i
-    console.log(`${numero_tabuada} X ${i} = ${resultado}`)
-}
